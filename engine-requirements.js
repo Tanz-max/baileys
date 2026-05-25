d2b14fe3fc511e0b2a587688546c08af135754d1
Tanz-max <fatanqinar@gmail.com>
2026-05-25 14:05:01 +0700

    Update engine-requirements.js

--- engine-requirements.js
+++ engine-requirements.js
@@ -2,7 +2,7 @@ const major = parseInt(process.versions.node.split('.')[0], 10);
 
 if (major < 20) {
   console.error(
-    `\n❌ Oops! Node.js kamu masih jadul 😅\n` +
+    `\n❌ Oops! Node.js kamu masih jadul\n` +
     `   Versi sekarang : ${process.versions.node}\n` +
     `   Minimal        : Node.js 20+\n\n` +
     `   Upgrade dulu ya biar lancar ✨\n`
